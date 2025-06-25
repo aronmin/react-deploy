2f2ed5ca0e0270d47fae19a3694ee18ac111070d
aronmin <scm0418@naver.com>
2025-06-25 09:23:39 +0900

    react upload

---
+++ src/stores/AuthStore.js
@@ -0,0 +1,27 @@
+import {create} from 'zustand'
+import {persist} from "zustand/middleware";
+import useGameStore from "./GameStore";
+
+const useAuthStore = create()(
+    persist(
+        (set) => ({
+          isLoggedIn: false,
+          username: "",
+          roles: "",
+          accessToken: "",
+
+          login: (claims, token) => set({
+            isLoggedIn: true,
+            username: claims.sub, roles: claims.roles.split(","), accessToken: token,
+          }),
+
+          logout: () => set({
+            isLoggedIn: false,
+            username: "", roles: [], accessToken: ""
+          }),
+        }),
+        {name:'auth-storage'}
+    ),
+)
+
+export default useAuthStore
